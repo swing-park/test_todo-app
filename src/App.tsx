2e22143b7bcb69b1d81f011e6f78693bd8782d22
dev.swing <ongsim123@gmail.com>
2023-02-09 10:50:22 +0900

    Feat :sparkles: : todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+interface ICard {
+  text: string;
+}
+
+interface Props {
+  text: string;
+}
+
+const Card = ({ text }: Props) => {
+  return <div className="card">{text}</div>;
+};
+
+const App = () => {
+  const [cards, setCards] = useState<ICard[]>([{ text: "Init" }]);
+  const [text, setText] = useState<string>("");
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setCards([...cards, { text }]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" id="input" onChange={handleOnChange} required />
+        <button type="submit">추가하기</button>
+      </form>
+      <div className="title">Todo List</div>
+      <div className="cardList">
+        {cards.map((card, idx) => (
+          <Card key={idx} text={card.text} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
